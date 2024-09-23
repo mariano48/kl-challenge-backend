c5a1c2d7ebc01e9108d24bada990965c549b2d0f
mariano48 <marianosolis423@gmail.com>
2024-09-23 12:45:32 -0300

    Add movies controllers

---
+++ src/modules/movies/controller.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import {
+  getMoviesSuggestionsFromQuery,
+  getMoviesWithGeolocations,
+} from "./service";
+
+export interface MoviesQueryParams {
+  query: {
+    title?: string;
+  };
+}
+
+export const getMovies = async (
+  req: Request<MoviesQueryParams>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { title } = req.query;
+
+  try {
+    res.json(
+      await getMoviesWithGeolocations({
+        title: title as string,
+      })
+    );
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+export const getSuggestions = async (req: Request, res: Response) => {
+  try {
+    const { title } = req.query;
+    res.json(await getMoviesSuggestionsFromQuery({ title: title as string }));
+  } catch (error) {
+    res.send(error.message);
+  }
+};
