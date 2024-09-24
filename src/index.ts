39866a2f3ce3efc56ea41cbf5badd1805f12510f
mariano48 <marianosolis423@gmail.com>
2024-09-24 07:41:15 -0300

    Fix dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
+require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import { getMovies, getSuggestions } from "./modules/movies/controller";
 
-const PORT = 9000;
+const PORT = process.env.PORT || 6000;
 const server = express();
 
 server.use(cors());
 server.use(express.json());
+
 server.get("/movies", getMovies);
 server.get("/suggestions", getSuggestions);
 server.get("/", (req, res) => res.send("OK"));
